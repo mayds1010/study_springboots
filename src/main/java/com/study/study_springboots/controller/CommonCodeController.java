5806a34328018a0dea14bd9379a9a810707f7402
mayds1010 <mayds1010@gmail.com>
2023-01-17 15:53:53 +0900

    commit

--- src/main/java/com/study/study_springboots/controller/CommonCodeController.java
+++ src/main/java/com/study/study_springboots/controller/CommonCodeController.java
@@ -4,13 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.study.study_springboots.dao.CommonCodeDao;
+
 @Controller
 @RequestMapping(value = "/commonCode")
 public class CommonCodeController {
 
     @Autowired
+    CommonCodeDao commonCodeDao;
+
     @RequestMapping(value = "/list") // http://localhost:8080/home
-    public void home() {
+    public void list() {
         // Object result = CommonCodeDao.getList();
         int i = 0;
     }
